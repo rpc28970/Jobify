0c53f81f8904066c61afa912835b855d83ff4721
rpc28970 <rpc28970@gmail.com>
2023-02-09 16:11:52 -0500

    Landing Page, Logo Component, Component Index

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,43 @@
+import React from "react";
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu
+            sapien ut magna eleifend mollis. Proin sem lorem, faucibus sit amet
+            dictum eu, dictum quis purus. Quisque mollis nulla nisl, sit amet
+            fringilla arcu faucibus quis. Maecenas dapibus molestie nibh vitae
+            efficitur. Phasellus in sollicitudin nisi. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit. Donec mi tellus, aliquam vel
+            hendrerit in, gravida at diam. Vestibulum at ipsum nec massa maximus
+            maximus. In sit amet auctor felis, non elementum mi. Duis in ligula
+            tristique est accumsan venenatis. Nunc pharetra ipsum vel diam
+            consequat, in iaculis libero rhoncus. Fusce sit amet blandit diam.
+            Etiam sit amet luctus purus. Donec ut libero eu odio tempor blandit
+            et vel ex. Sed non dolor eleifend, venenatis lorem sit amet, euismod
+            quam.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        {/* image */}
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
