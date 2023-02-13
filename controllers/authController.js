7a928616aacb9aa27cac51d65e852dd25a80ae9c
rpc28970 <rpc28970@gmail.com>
2023-02-13 11:11:53 -0500

    Server Side Error Handling, Custom Api Errors

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,21 @@
 import User from "../models/User.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError } from "../errors/index.js";
 
 const register = async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    res.status(201).json({ user });
-  } catch (error) {
-    res.status(500).json({ msg: 'There was an error' });
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError("please provide all the values.");
   }
+
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError("Email already in use.");
+  }
+
+  const user = await User.create(name, email, password);
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
