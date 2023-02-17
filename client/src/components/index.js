a754d0772f14dea584db4cc10d5b3cd5a92555e5
rpc28970 <rpc28970@gmail.com>
2023-02-17 10:33:58 -0500

    Create / Add Job Functionality Complete

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,5 +4,14 @@ import Alert from "./Alert";
 import Navbar from "./Navbar";
 import BigSidebar from "./BigSidebar";
 import SmallSidebar from "./SmallSidebar";
+import FormRowSelect from "./FormRowSelect";
 
-export { Logo, FormRow, Alert, Navbar, SmallSidebar, BigSidebar };
+export {
+  Logo,
+  FormRow,
+  Alert,
+  Navbar,
+  SmallSidebar,
+  BigSidebar,
+  FormRowSelect,
+};
--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,15 +1,31 @@
+import Job from "../models/Job.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, NotFoundError } from "../errors/index.js";
+
 const createJob = async (req, res) => {
-  res.send("create job");
+  const { position, company } = req.body;
+  if (!position || !company) {
+    throw new BadRequestError("Please provide all values");
+  }
+  req.body.createdBy = req.user.userId;
+
+  const job = await Job.create(req.body);
+  
+  res.status(StatusCodes.CREATED).json({ job });
 };
+
 const deleteJob = async (req, res) => {
   res.send("delete job");
 };
+
 const getAllJobs = async (req, res) => {
   res.send("get all jobs");
 };
+
 const updateJob = async (req, res) => {
   res.send("update job");
 };
+
 const showStats = async (req, res) => {
   res.send("show stats");
 };
